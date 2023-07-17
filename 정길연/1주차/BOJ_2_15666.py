37a040f940e3ee7df08967f65d03b383b662ca04
gilyeon00 <12venus08@naver.com>
2023-07-17 20:17:55 +0900

    기업 코테 답안 추가

--- 정길연/1주차/BOJ_2_15666.py
+++ 정길연/1주차/BOJ_2_15666.py
@@ -1,22 +1,20 @@
 n, m = map(int, input().split())
 
 li = list(map(int, input().split()))
-dup_check=[]
 
+li = list(set(li))      # 중복제거 
+li.sort()               # 오름차순 1 7 9
 
-def dfs(num, start, ans):
+def dfs(num, start, ans): 
     if len(ans) == num:
         for i in ans:
-            print(li[i],end=' ')
+            print(li[i], end=' ')
         print()
         
         return
     
-    for i in range(start, len(li)):
-        dfs(num,i,ans+[i])
-
-li = list(set(li))
-li.sort()
+    for i in range(start, len(li)):  
+        dfs(num, i, ans+[i])
 
 for i in range( len(li) ):
-    dfs(m, i, [i])
+    dfs(m, i, [i])    
