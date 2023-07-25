59adf8f73dc4b1e87895443d96c634875a9b9f09
gilyeon00 <12venus08@naver.com>
2023-07-25 21:03:36 +0900

    3주차 1213번 문제풀이 (완)

--- 정길연/3주차/BOJ_1_1213.py
+++ 정길연/3주차/BOJ_1_1213.py
@@ -3,7 +3,7 @@ from collections import Counter
 name_list = list(input())
 names = Counter(name_list)
 
-names.sort()
+name_list.sort()
 
 cnt = 0
 for item in names:
@@ -14,5 +14,15 @@ for item in names:
     if cnt > 1:
         break
 
+if cnt > 1:
+    print("I'm Sorry Hansoo")
+else:
+    ans = ""
+    for i in range(0, len(name_list), 2):
+        ans += name_list[i]
+        
+    print (ans + center + ans[::-1])
     
+
+
 
