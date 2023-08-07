6951957fec66213df04a9e0fb93495dfec2a8346
zyyyyysoo <newsty1107@gmail.com>
2023-08-07 12:11:54 +0900

    3주차 풀이 업데이트

--- jjs/3주차/1213.py
+++ jjs/3주차/1213.py
@@ -3,26 +3,26 @@ from collections import Counter
 N = list(map(str, input()))
 N.sort()
 
-cnt = Counter(N)
+cnt = Counter(N) # 각 알파벳의 개수 세어줌
 odd = 0
 c_word = ""
 
 for i in cnt:
-    if cnt[i] % 2 == 1:
-        odd += 1
-        c_word += i
-        N.remove(i)
+    if cnt[i] % 2 == 1: #만약 홀수라면
+        odd += 1 # 홀수가 몇개인지 세어줌
+        c_word += i # 홀수인 문자 더해줌
+        N.remove(i) # 사용한 문자 제거
 
-    if odd > 1:
+    if odd > 1: # 만약 홀수가 여러개라면 넌 탈락
         print("I'm Sorry Hansoo")
         break
 
-result = ""
+result = "" # 글자를 넣을 빈 문자열
 
-if odd == 1:
+if odd == 1: # 만약 홀수가 한개라면 가운데 글자가 정해지고
     words = ""
-    for i in range(0, len(N), 2):
+    for i in range(0, len(N), 2): # 같은글자 두개씩 반복이니까 간격 지정해서 더해주기
         words += N[i]
-    result = words + c_word + words[::-1]
+    result = words + c_word + words[::-1] # 가운데 글자 넣고 거꾸로 반복
 
 print(result)
--- jjs/3주차/2002.py
+++ jjs/3주차/2002.py
@@ -6,16 +6,16 @@ arr = []
 
 for i in range(N):
     car = input()
-    dict[car] = i
+    dict[car] = i # 들어가는 차 저장
 
 for _ in range(N):
     car = input()
-    arr.append(car)
+    arr.append(car) # 나오는 차 저장
 
-for k in range(N - 1):
+for k in range(N - 1): # 나오는 차를 탐색
     for h in range(k + 1, N):
-        if dict[arr[k]] > dict[arr[h]]:
-            result += 1
+        if dict[arr[k]] > dict[arr[h]]: # 현재 자동차의 인덱스보다 작은 차가 있을 경우
+            result += 1 # 카운트해준다
             break
 
 print(result)
