35151c13f44a8bb44a10e355ba1312f2198b26e3
zyyyyysoo <newsty1107@gmail.com>
2023-08-09 00:21:21 +0900

    5주차 풀이 주석 추가

--- jjs/5주차/11722.py
+++ jjs/5주차/11722.py
@@ -1,10 +1,10 @@
 N = int(input())
 lst = list(map(int, input().split()))
 
-dp = [1] * N
+dp = [1] * N # 혼자일 때는 길이가 1이므로 1로 초기화
 for i in range(N):
     for j in range(i):
-        if lst[i] < lst[j]:
-            dp[i] = max(dp[i], dp[j] + 1)
+        if lst[i] < lst[j]: # 감소하려면 앞의 수가 다음 수보다 커야함
+            dp[i] = max(dp[i], dp[j] + 1) # 앞 숫자의 부분 수열 길이에 1을 더한 값이 나의 부분 수열 길이이고 이 중 가장 긴 값이 dp
 
 print(max(dp))
--- jjs/5주차/2156.py
+++ jjs/5주차/2156.py
@@ -7,9 +7,13 @@ for i in range(N):
 dp = [0] * 10001
 dp[0] = arr[0]
 dp[1] = arr[0] + arr[1]
-dp[2] = max(dp[1], arr[0] + arr[2], arr[1] + arr[2])
+dp[2] = max(dp[1], arr[0] + arr[2], arr[1] + arr[2]) # 연속 3잔을 모두 마실 수 없기 때문에 선택
 
-for i in range(3, N):
+for i in range(3, N): # 4잔 이상일 경우 고려해야 할 것들
+    # 마지막 잔을 마시지 않는 경우 -> dp[i - 1]
+    # 두번째 잔 까지 선택한 후 세번째를 건너 뛰고 마지막 잔 마시는 경우 -> dp[i - 2] + arr[i]
+    # 두번째 잔 건너 뛰고 세번째 잔과 마지막 잔을 마시는 경우 -> dp[i - 3] + arr[i - 1] + arr[i]
     dp[i] = max(dp[i - 1], dp[i - 2] + arr[i], dp[i - 3] + arr[i - 1] + arr[i])
 
+# 이 중 최대가 되는 dp를 구하면 됨
 print(max(dp))
